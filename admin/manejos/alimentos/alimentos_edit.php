db642e5d029ab10d1a7f313115e05fe1d176fe8b
Juan5-3 <juandsn03@gmail.com>
2025-08-21 17:47:11 -0300

    Adicionei html a alimentos e procedimentos

---
+++ admin/manejos/alimentos/alimentos_edit.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Editar Alimento</title>
+</head>
+<body>
+    <button class="voltar-btn" onclick="window.history.back();">← Voltar</button>
+
+    <h1>Editar Alimento</h1>
+    <form method="post">
+        <input type="hidden" name="id" value="<?php echo htmlspecialchars($row['id']); ?>">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" value="<?php echo htmlspecialchars($row['nome']); ?>" required>
+        <label for="nome">Descrição:</label>
+        <input type="text" id="descricao" name="descricao" value="<?php echo htmlspecialchars($row['descricao']); ?>" required>
+        <button type="submit">Salvar</button>
+    </form>
+</body>
+</html>
